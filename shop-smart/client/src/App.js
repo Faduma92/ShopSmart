61b3ac36de1be1026124b0f9897f0312f1f6692a
Faduma Abdillahi <faduma92@gmail.com>
2020-10-29 21:33:59 -0400

    added carousel with 3 images

--- shop-smart/client/src/App.js
+++ shop-smart/client/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MyNav from './components/Nav/Nav'
-
+import MyCarousel from './components/Carousel/Carousel'
 
 function App() {
   return (
     
 <div>
    <MyNav />
+   <MyCarousel />
 </div>
   );
 }
