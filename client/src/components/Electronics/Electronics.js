0930277accd582103b4201561b52fe217bfb372f
Faduma Abdillahi <faduma92@gmail.com>
2020-11-06 17:43:16 -0500

    adding catergory api

--- client/src/components/Electronics/Electronics.js
+++ client/src/components/Electronics/Electronics.js
@@ -1,12 +1,87 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Paper from "@material-ui/core/Paper";
+import { Card, Button } from "react-bootstrap";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import Icon from "@material-ui/core/Icon";
+import { useEffect, useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 300,
+      width: 200,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
 
 export default function Electronics(props) {
     
 
+    const [spacing, setSpacing] = React.useState(10);
+    const classes = useStyles();
+    const [product, setProduct] = useState([]);
+    const [justforyou, setJustforyou] = useState([]);
+  
+    function getProducts() {
+      return fetch("/product").then((data) => data.json());
+    }
+  
+    useEffect(() => {
+      getProducts().then((data) => {
+        const newArray = data.splice(0, 5);
+        console.log(data);
+        setProduct(newArray);
+      });
+    }, []);
+  
+    useEffect(() => {
+      getProducts().then((data) => {
+        const newArray = data.splice(13, 16);
+        console.log(data);
+        setJustforyou(newArray);
+      });
+    }, []);
+  
     return (
-        <>
-           Hello Electronics 
-        </>
-    )
-}
+      <div className="latest-product-area">
+        <h3 className="section-tittle text-center mb-50">Electronics</h3>
+        <Grid container className={classes.root} spacing={5}>
+          <Grid item xs={12}>
+            <Grid container justify="center" spacing={spacing}>
+              {product.map((value) => (
+                <Grid key={value._id} item>
+                  <Paper className={classes.paper}>
+                    <Card.Img variant="top" src={value.productimage} />
+                    <Card.Body>
+                      <Card.Title>{value.productname}</Card.Title>
+                      <Card.Text>$ {value.price}</Card.Text>
+                      <IconButton aria-label="add to favorites">
+                        <Icon>add_circle</Icon>
+                      </IconButton>
+                    </Card.Body>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+  
+          
+        </Grid>
+
+      </div>
+    );
+  }
+  
+
 
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,35 +1,27 @@
-import React from 'react';
-import Nav from 'react-bootstrap/Nav'
-import './Footer.css'
-
-
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import "./Footer.css";
 
 function Footer() {
-    return (
-
-  <div className="footer footer-area footer-padding">
+  return (
+    <div className="footer footer-area footer-padding">
       <Nav className="justify-content-center" activeKey="/home">
-    <Nav.Item>
-      <Nav.Link href="/home">About Us</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="link-1">Contact Us</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="link-2">Policy</Nav.Link>
-    </Nav.Item>
-   
-  </Nav>
-  <p className="text-center mt-4 mb-4"> Copyright &copy;
-                              
-                                | All rights reserved | Shop Smart</p>
-  
-  
-  
-
-  </div>
-);
-
+        <Nav.Item>
+          <Nav.Link href="/home">About Us</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Contact Us</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Policy</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <p className="text-center mt-4 mb-4">
+        {" "}
+        Copyright &copy; | All rights reserved | Shop Smart
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
