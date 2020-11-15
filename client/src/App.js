188efce6648ce98376804f74832fec79c0204f09
Faduma Abdillahi <faduma92@gmail.com>
2020-11-15 10:05:36 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import MyNav from './components/Nav/Nav'
-import Home from './components/Home/Home'
-import Footer from './components/Footer/Footer'
-import Electronics from './components/Electronics/Electronics'
-import Health from './components/Health/Health'
-import Beauty from './components/Beauty/Beauty'
-import Aboutus from './components/Aboutus/Aboutus'
-import Cart from './components/Cart/Cart'
+import logo from "./logo.svg";
+import "./App.css";
+import MyNav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Electronics from "./components/Electronics/Electronics";
+import Health from "./components/Health/Health";
+import Beauty from "./components/Beauty/Beauty";
+import Aboutus from "./components/Aboutus/Aboutus";
+import Cart from "./components/Cart/Cart";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  BrowserRouter
+  Redirect,
+  BrowserRouter,
 } from "react-router-dom";
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
 
 function App() {
   return (
-    
-<div>
-   <BrowserRouter>
-   <MyNav />
-   <Switch>
-     <Route exact path="/">
-      
-
-  <Home />
-     </Route>
-     <Route path="/Electronics">
-<Electronics />
-     </Route>
-     <Route path="/Health">
-<Health />
-     </Route>
-     <Route path="/Beauty">
-<Beauty />
-     </Route>
-     <Route path="/Cart">
-<Cart />
-     </Route>
-     
-     <Route path="/Aboutus">
-<Aboutus />
-     </Route>
-  
-   </Switch>
-   <Footer />
-   </BrowserRouter>
-   
-</div>
+    <div>
+      <BrowserRouter>
+        <MyNav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Electronics">
+            <Electronics />
+          </Route>
+          <Route path="/Health">
+            <Health />
+          </Route>
+          <Route path="/Beauty">
+            <Beauty />
+          </Route>
+          <Route path="/Cart">
+            <Cart />
+          </Route>
+          <Route path="/Aboutus">
+            <Aboutus />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/components/Aboutus/Aboutuscard.js
+++ client/src/components/Aboutus/Aboutuscard.js
@@ -7,7 +7,7 @@ function Aboutuscard({ name, linkedin, github, image }) {
   return (
     <div>
       <Card className="cardstyle">
-        <Card.Img variant="top" src={image} style={{width: "300px", height:"200px"}}/>
+        <Card.Img variant="top" src={image} style={{width: "350px", height:"200px"}}/>
         <Card.Body>
           <Card.Title>
             <h4>Hi, I am: {name}</h4>
